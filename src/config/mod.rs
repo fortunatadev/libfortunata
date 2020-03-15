8f099b3e62ea10109642370e1bf2a99bc98ee0f5
Carrie J V <carrie@carriejv.com>
2020-03-15 11:19:23 -0700

    Fix file structure, comments, minor issues

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -4,26 +4,52 @@ use std::error;
 use std::fmt;
 use std::fs;
 use std::io;
-use std::path;
 
 // --- Consts
 /// Version identifier
-const CFG_VERSION: &str = "1.0";
+pub const CFG_VERSION: &str = "1.0";
 /// Config file name
-const CFG_FILE_NAME: &str = "vanguard.toml";
+pub const CFG_FILE_NAME: &str = "vanguard.toml";
 
-/// Loads and parses config from the `vanguard.toml` file in the application root.
-/// All errors are silently ignored, preferring to return default config.
-pub fn load_config() -> Result<Config, ConfigError> {
-    let mut cfg_file_path = std::env::current_dir()?;
-    Ok(Default::default())
+/// Returns hardcoded default config
+pub fn get_default_config() -> Config {
+    Default::default()
 }
 
+/// Reads config entires from a `vanguard.toml` file.
+pub fn read_config_file() -> Result<Config, ConfigError> {
+    // Build file path
+    let mut cfg_file_path = std::env::current_dir()?;
+    cfg_file_path.push(CFG_FILE_NAME);
+    // Read file
+    let cfg_file_contents = fs::read_to_string(cfg_file_path)?;
+    // Attempt toml parse
+    let config = toml::de::from_str(&cfg_file_contents)?;
+    Ok(config)
+}
+
+/// Writes a Config object back to disk as `vanguard.toml`.
+pub fn write_config_file(config: &Config) -> Result<(), ConfigError> {
+    // Build file path
+    let mut cfg_file_path = std::env::current_dir()?;
+    cfg_file_path.push(CFG_FILE_NAME);
+    // Serialize current config
+    let cfg_toml = toml::ser::to_string(config)?;
+    // Write file
+    fs::write(cfg_file_path, cfg_toml)?;
+    Ok(())
+}
+
+// fn get_cfg_file_path() -> String {
+
+// }
 
 /// Config file data model
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(default)]
 pub struct Config {
+    /// Version identifier for config file
+    version: String,
     /// Default download path for new manifests. Default is app root.
     download_path: Option<String>,
     /// Maximum parallel file workers to use.
@@ -32,15 +58,16 @@ pub struct Config {
     use_symlinked_storage: bool,
     /// Array-table of manifests in use
     #[serde(rename = "manifest")]
-    manifests: Option<Vec<ManifestConfig>>,
+    manifests: Vec<ManifestConfig>,
 }
 impl Default for Config {
     fn default() -> Config {
         Config {
+            version: CFG_VERSION.to_owned(),
             download_path: None,
             maximum_parallel_files: 4,
             use_symlinked_storage: true,
-            manifests: None
+            manifests: Vec::new()
         }
     }
 }
@@ -50,7 +77,7 @@ impl Default for Config {
 #[serde(default)]
 pub struct ManifestConfig {
     /// URL of the manifest. Config entires without a manifest URL will be ignored.
-    url: Option<String>,
+    url: String,
     /// If true, allows patching from non-https mirrors.
     allow_insecure_patching: bool,
     /// Download path for the specific manifest. Overrides global setting. Default is app root.
@@ -63,7 +90,7 @@ pub struct ManifestConfig {
 impl Default for ManifestConfig {
     fn default() -> ManifestConfig {
         ManifestConfig {
-            url: None,
+            url: String::new(),
             allow_insecure_patching: false,
             download_path: None,
             ignore_checksum: false,
@@ -77,12 +104,16 @@ impl Default for ManifestConfig {
 pub enum ConfigError {
     FileIO(io::Error),
     InvalidConfig(String),
+    InvalidModel(toml::ser::Error),
+    InvalidSyntax(toml::de::Error)
 }
 impl fmt::Display for ConfigError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ConfigError::FileIO(ref e) => e.fmt(f),
-            ConfigError::InvalidConfig(ref desc) => write!(f, "Invalid config - {}", desc)
+            ConfigError::InvalidConfig(ref desc) => write!(f, "Invalid config - {}", desc),
+            ConfigError::InvalidModel(ref e) => e.fmt(f),
+            ConfigError::InvalidSyntax(ref e) => e.fmt(f)
         }
     }
 }
@@ -90,12 +121,24 @@ impl error::Error for ConfigError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match *self {
             ConfigError::FileIO(ref e) => Some(e),
-            ConfigError::InvalidConfig(ref _desc) => None
+            ConfigError::InvalidConfig(ref _desc) => None,
+            ConfigError::InvalidModel(ref e) => Some(e),
+            ConfigError::InvalidSyntax(ref e) => Some(e),
         }
     }
 }
 impl From<io::Error> for ConfigError {
     fn from(item: io::Error) -> ConfigError {
         ConfigError::FileIO(item)
+    }
+}
+impl From<toml::ser::Error> for ConfigError {
+    fn from(item: toml::ser::Error) -> ConfigError {
+        ConfigError::InvalidModel(item)
+    }
+}
+impl From<toml::de::Error> for ConfigError {
+    fn from(item: toml::de::Error) -> ConfigError {
+        ConfigError::InvalidSyntax(item)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 // --- Modules
-mod config;
-mod manifest;
+pub mod config;
+pub mod manifest;
 
 fn main() {
-	// Get Manifest
-	let http_manifest = ureq::get(&std::env::var("VG_MANIFEST").unwrap()).call();
-	if http_manifest.ok() {
-        println!("Got it!");
-        let manifest = manifest::deserialize_manifest(&http_manifest.into_string().unwrap()).unwrap();
-        println!("{:#?}", manifest);
-        let serialized_as_toml = manifest::manifest_spec::vg_1_0::serialize_manifest(&manifest).unwrap();
-        println!("{}", serialized_as_toml);
-        let parsed_as_toml = manifest::deserialize_manifest(&serialized_as_toml).unwrap();
-        println!("{:#?}", parsed_as_toml);
-    }
-    else {
-        panic!("Manifest get failed.");
-    }
+    let def = config::get_default_config();
+    let cfg = config::read_config_file();
+
+    println!("{:#?}", def);
+    println!("{:#?}", cfg);
+
+    config::write_config_file(&def).unwrap();
+	// // Get Manifest
+	// let http_manifest = ureq::get(&std::env::var("VG_MANIFEST").unwrap()).call();
+	// if http_manifest.ok() {
+    //     println!("Got it!");
+    //     let manifest = manifest::deserialize_manifest(&http_manifest.into_string().unwrap()).unwrap();
+    //     println!("{:#?}", manifest);
+    //     let serialized_as_toml = manifest::manifest_spec::vg_1_0::serialize_manifest(&manifest).unwrap();
+    //     println!("{}", serialized_as_toml);
+    //     let parsed_as_toml = manifest::deserialize_manifest(&serialized_as_toml).unwrap();
+    //     println!("{:#?}", parsed_as_toml);
+    // }
+    // else {
+    //     panic!("Manifest get failed.");
+    // }
 }
